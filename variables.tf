00adfdf6a75a8b70be3a62c04edb4d6929d8ea8e
chinny247 <94147602+chinny247@users.noreply.github.com>
2022-10-20 06:41:36 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "name" {
+    type=string
+    default="example" 
+}
+variable "location" {
+  type=string
+  default="example"
+}
+variable "vnet_name" {
+  type=string
+  default="example"
+}
+variable "vnet_address_space" {
+  default="example"
+}
+variable "subnet_name" {
+  type=string
+  default="example"
+}
+variable "subnet_address_prefix" {
+  default="example"
+}
